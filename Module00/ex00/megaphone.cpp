3fde52e78bb5ea222f87a0cabfc8a99b8964a38e
Burak Yıldırım <burakyldrm0@icloud.com>
2023-02-10 13:46:05 +0300

    kodda yer alan eksikler giderildi

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -2,24 +2,25 @@
 
 int main(int argc, char **argv)
 {
-    int i = 1;
-    int j = 0;
+	int i = 1;
+	int j = 0;
 
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    else
-    {
-        while (argv[i])
-        {
-            while (argv[i][j])
-            {
-                std::cout << (char)toupper(argv[i][j]);
-                j++;
-            }
-            i++;
-            j = 0;
-        }
-    }
-    std::cout << std::endl;
-    return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (argv[i])
+		{
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			std::cout << " ";
+			i++;
+			j = 0;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
