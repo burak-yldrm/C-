44791e6fc36a8865521d069bbc4f964f15ecd98a
Burak Yıldırım <burakyldrm0@icloud.com>
2023-02-14 11:58:22 +0300

    HumanB sınıfı üyelerinden olan setWeapon fonksiyonu referans değerden pointer'a çekildi.

--- Module01/ex03/main.cpp
+++ Module01/ex03/main.cpp
@@ -14,9 +14,10 @@ int	main()
 	{
 		Weapon	club = Weapon("crude spiked club");
 		HumanB	jim("Jim");
-		jim.setWeapon(club);
+		jim.setWeapon(&club);
 		jim.attack();
 		club.setType("some other type of club");
+		jim.setWeapon(NULL);
 		jim.attack();
 	}
 }
